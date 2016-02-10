3da5516fa20a423cd5c9a1bafdc70bb7719f0f28
vetruvian <cory.malstrom@yahoo.com>
2016-02-10 13:37:58 -0800

    Routed pages#home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+
   devise_for :users, controllers: { registrations: 'registrations'}
-  root 'welcome#index'
+  #root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
