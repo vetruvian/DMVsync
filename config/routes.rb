dad14f8bd25834eafce1b0b5e0e40aaed90f0ddd
vetruvian <cory.malstrom@yahoo.com>
2016-02-10 15:11:06 -0800

    Put forms in a dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'registrations'}
   root 'pages#home'
 
-  devise_for :users, controllers: { registrations: 'registrations'}
   #root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
